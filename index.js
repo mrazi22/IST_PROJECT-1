3b5457710b1ff12ec3c609cd7e16278506cf5e76
Ziri254 <simongunyali@gmail.com>
2024-07-28 22:04:18 +0300

    darkmode

--- index.js
+++ index.js
@@ -57,6 +57,17 @@
   Swal.fire('Success', 'Message sent successfully', 'success');
 }
 
+// dark mode
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+    icon.src = "/assets/sun.png";
+  }else{
+    icon.src = "/assets/moon.png";
+  }
+}
 
 
 
